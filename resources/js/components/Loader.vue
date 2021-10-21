3f2de1cd9afc567f83ee1e00c6c887846df0fa08
Roccofranchini <rocco.franchini1@gmail.com>
2021-10-21 14:55:12 +0200

    spinner-added

---
+++ resources/js/components/Loader.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="loader d-flex justify-content-center align-items-center">
+        <div class="spinner-border" role="status">
+            <span class="sr-only">Loading...</span>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Loader"
+};
+</script>
+
+<style scoped lang="scss">
+.loader {
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background-color: rgba(0, 0, 0, 0.2);
+
+    .spinner-border {
+        width: 200px;
+        height: 200px;
+    }
+}
+</style>
--- resources/js/components/Main.vue
+++ resources/js/components/Main.vue
@@ -4,38 +4,45 @@
             <h1 class="text-center my-3">ARTICOLI</h1>
             <div class="row flex-column">
                 <PostCard v-for="post in posts" :key="post.id" :post="post" />
+                <Loader v-if="isLoading" />
             </div>
         </div>
     </main>
 </template>
 
 <script>
-
 import axios from "axios";
-import PostCard from "./PostCard.vue"
+import PostCard from "./PostCard.vue";
+import Loader from "./Loader.vue";
 
 export default {
     name: "Main",
     components: {
         PostCard,
+        Loader
     },
     data() {
         return {
             baseUri: "http://localhost:8000",
-            posts: []
+            posts: [],
+            isLoading: false
         };
     },
     methods: {
         getPosts() {
+            this.isLoading = true;
             axios
                 .get(`${this.baseUri}/api/posts`)
-                .then((res) => {
+                .then(res => {
                     this.posts = res.data;
                 })
-                .catch((err) => {
+                .catch(err => {
                     console.error(err);
+                })
+                .then(() => {
+                    this.isLoading = false;
                 });
-        },
+        }
     },
     created() {
         this.getPosts();
